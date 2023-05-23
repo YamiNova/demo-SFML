7f1278cc13fc5595c0cdb127d17f56fba75a4c51
xhy <hangyu_xia@outlook.com>
2023-05-23 23:41:52 +0800

    change window size

--- test-sfml.cpp
+++ test-sfml.cpp
@@ -1,7 +1,7 @@
 #include <SFML/Graphics.hpp>
 
 int main(){
-    sf::RenderWindow window(sf::VideoMode(400, 400), "SFML works!");
+    sf::RenderWindow window(sf::VideoMode(500, 400), "SFML works!");
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Red);
     while (window.isOpen())
